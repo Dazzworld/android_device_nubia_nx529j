2d4e1b26c80ba1b6bdaf44b8bada8aac176d2033
dazzworld@163.com <dazzworld@163.com>
2016-12-12 23:58:44 +0800

    remove unused files

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,19 +16,6 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    android/sensor.cpp \
-    gui/SensorManager.cpp
-
-LOCAL_C_INCLUDES := gui
-LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid
-LOCAL_MODULE := libshim_sensor
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils libui
 LOCAL_SRC_FILES := shim_camera.c
 LOCAL_MODULE := libshim_camera
