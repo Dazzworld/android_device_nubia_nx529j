75e9d1ff659ec3ffef1499b254e22f6861155d81
ChenZhichao <dazzworld@163.com>
2017-02-05 20:48:33 +0800

    dont buid keyhandler

--- device.mk
+++ device.mk
@@ -311,13 +311,13 @@ PRODUCT_PACKAGES+=\
 #	ifaa_fingerprint
 
 # KeyHandler
-PRODUCT_PACKAGES += \
-    com.cyanogenmod.keyhandler
+#PRODUCT_PACKAGES += \
+ #   com.cyanogenmod.keyhandler
 
-PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
+#PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
 
 # never dexopt the keyhandler
-$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
+#$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
 
 # Wifi
 PRODUCT_PACKAGES += \
